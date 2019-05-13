e277d9f925a010eb5b7c3562360120fa44faa4bc
Pedrovyski <50590818+Pedrovyski@users.noreply.github.com>
2019-05-13 13:42:04 -0300

    Add files via upload

--- dados.py
+++ dados.py
@@ -2,7 +2,16 @@
 
 from random import randint # Importa a função de gerar números aleatórios
 
-print("Olá 1Info2!")
+print("Jogo dos dados")
 
 dado1 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ")
+print("Dado 1: ", dado1)
+
+dado2 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado2)
+
+dado3 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado3)
+
+dado4 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 4: ", dado4)
