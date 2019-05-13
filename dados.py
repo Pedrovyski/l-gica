dfed3e44a6a784c19606ad208973108621e4b85e
Pedrovyski <50590818+Pedrovyski@users.noreply.github.com>
2019-05-13 14:19:01 -0300

    Add files via upload
    Variáveis de vitória/empate incluídas

--- dados.py
+++ dados.py
@@ -1,4 +1,4 @@
-# Meu primeiro programa
+# Meu primeiro programa  
 
 from random import randint # Importa a função de gerar números aleatórios
 
@@ -8,10 +8,24 @@ dado1 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
 print("Dado 1: ", dado1)
 
 dado2 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado2)
+print("Dado 2: ", dado2)
 
 dado3 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado3)
+print("Dado 3: ", dado3)
 
 dado4 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
 print("Dado 4: ", dado4)
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print("jogador1: ", jogador1)
+print("jogador2: ", jogador2)
+
+if jogador1 > jogador2:
+    print("Jogador 1 venceu!")
+else:
+    if jogador2 > jogador1:
+        print("Jogador 2 venceu!")
+    else:
+        print("Empate")
