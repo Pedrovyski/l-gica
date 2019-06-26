3c3fafabb2ada37ee80b733a388f182dd2a9ac0a
Pedrovyski <50590818+Pedrovyski@users.noreply.github.com>
2019-06-26 14:26:59 -0300

    Add files via upload

---
+++ l-gica-master/dados.py
@@ -0,0 +1,31 @@
+# Meu primeiro programa  
+
+from random import randint # Importa a função de gerar números aleatórios
+
+print("Jogo dos dados")
+
+dado1 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado1)
+
+dado2 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 2: ", dado2)
+
+dado3 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 3: ", dado3)
+
+dado4 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 4: ", dado4)
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print("jogador1: ", jogador1)
+print("jogador2: ", jogador2)
+
+if jogador1 > jogador2:
+    print("Jogador 1 venceu!")
+else:
+    if jogador2 > jogador1:
+        print("Jogador 2 venceu!")
+    else:
+        print("Empate")
